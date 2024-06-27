4756c6ea2310111dd4ba5ed7a8c4934170b05117
David <deeseinmedia@gmail.com>
2024-06-27 11:31:59 +0200

    first SQL file

---
+++ mySQL_Workshop_DEHAM14.sql
@@ -0,0 +1,8 @@
+-- get list from departments
+SELECT * FROM departments;
+
+-- retrieve department id and name
+SELECT dept_no, dept_name FROM departments;
+
+-- retrieve the department id and name where the department id is d005
+SELECT dept_no, dept_name FROM departments WHERE dept_no = 'd005';
