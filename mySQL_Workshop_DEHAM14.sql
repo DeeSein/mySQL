e335ed14e1e3d31cd4f7ae7ae25418bdfd351bac
David <deeseinmedia@gmail.com>
2024-06-27 14:45:08 +0200

    tryed few new commands

--- mySQL_Workshop_DEHAM14.sql
+++ mySQL_Workshop_DEHAM14.sql
@@ -66,3 +66,68 @@ FROM    employees
 LIMIT 5;
 
 
+-- get the first_name in upper case and last_name in lower case
+SELECT  UPPER(first_name) 'First Name',
+        LOWER(last_name) 'Last Name'
+FROM    employees
+LIMIT 5;
+
+
+-- get sample 10 records from salaries table
+SELECT * FROM salaries
+LIMIT 10;
+
+
+-- get the average of the salaries also show maximum and minimum
+SELECT  AVG(salary) 'Average Salary',
+        MAX(salary) 'Maximum Salary',
+        MIN(salary) 'Minimum Salary'
+FROM    salaries;
+
+
+-- get thte number of records from titles table
+SELECT count(*) 'Number of Titles' 
+FROM titles;
+
+
+-- get 5 samples from titles table
+SELECT * FROM titles
+LIMIT 5;
+
+
+-- get 25 samples from titles table without dublicates
+SELECT DISTINCT title
+FROM titles
+LIMIT 25;
+
+
+-- get the number of records from titles table where the title is engineer
+SELECT count(*) 'Number of Engineers'
+FROM titles
+WHERE title = 'Engineer';
+
+
+-- list of some other sql functions
+-- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+-- get the current date and time
+SELECT NOW() 'Current Date and Time';
+
+
+-- get the current date, time and time which is 50 years from now()
+SELECT NOW(), DATE_ADD(NOW(), INTERVAL 50 YEAR) '50 Years from Now';
+
+
+-- get first_name and last_name from employees table and the salarie from the salaries table
+-- where the first_name and last_name are the same
+SELECT  e.first_name,
+        e.last_name,
+        s.salary
+FROM    employees e
+        INNER JOIN salaries s
+            ON e.emp_no = s.emp_no
+WHERE   e.first_name = e.last_name
+LIMIT 10;
+
+
