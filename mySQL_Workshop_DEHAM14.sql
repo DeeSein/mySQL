8366956dfd258e69ec3b0badc42ed20544d4ed62
David <deeseinmedia@gmail.com>
2024-06-28 10:32:00 +0200

    adding more querys for testing

--- mySQL_Workshop_DEHAM14.sql
+++ mySQL_Workshop_DEHAM14.sql
@@ -131,3 +131,67 @@ WHERE   e.first_name = e.last_name
 LIMIT 10;
 
 
+-- get titels and last names from the top 5 employees with the highest salaries
+SELECT  e.first_name,
+        e.last_name,
+        s.salary
+FROM    employees e
+        INNER JOIN salaries s
+            ON e.emp_no = s.emp_no
+ORDER BY s.salary DESC
+LIMIT 5;
+
+
+-- get the schema of employees table
+DESCRIBE employees;
+
+
+-- get the schema of titels table
+DESCRIBE titles;
+
+
+INSERT INTO titles (emp_no, title, from_date)
+VALUES (
+    10001,
+    'AI Specialist',
+    '2023-06-26'
+);
+
+
+INSERT INTO titles (emp_no, title, from_date)
+VALUES (
+    10002,
+    'AI Specialist II',
+    '2023-06-26'
+);
+
+
+-- create a stored procedure to insert data into the titles table
+DELIMITER $$
+CREATE PROCEDURE insert_title(
+    IN p_emp_no INT,
+    IN p_title VARCHAR(50),
+    IN p_from_date DATE
+)
+BEGIN
+    insert into titles (emp_no, title, from_date)     VALUES (p_emp_no, p_title, p_from_date);
+    END;
+
+
+-- using the above stored procedure to insert data into the titles table
+CALL insert_title(10003, 'AI Specialist 3', '2023-06-26');
+CALL insert_title(10004, 'AI Specialist 4', '2023-06-26');
+CALL insert_title(10005, 'AI Specialist 5', '2023-06-26');
+CALL insert_title(10006, 'AI Specialist 6', '2023-06-26');
+CALL insert_title(10007, 'AI Specialist 7', '2023-06-26');
+CALL insert_title(10008, 'AI Specialist 8', '2023-06-26');
+CALL insert_title(10009, 'AI Specialist 9', '2023-06-26');
+CALL insert_title(10010, 'AI Specialist 10', '2023-06-26');
+CALL insert_title(10011, 'AI Specialist 11', '2023-06-26');
+
+
+-- get the list of titles from the titles table with titles like AI
+SELECT * FROM titles
+WHERE title LIKE '%AI%';
+
+
